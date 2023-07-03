db50a60ad829cf44f57dc8719d1a491ff0d8130f
Adrian <adrianvalverde45@gmail.com>
2023-07-03 12:11:13 -0400

    commit tree d44b6b3be39433ffa599a9b64687c58ee2141d42

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,8 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
+  extend Memorable
+
   @@songs = []
 
   def initialize
@@ -18,13 +20,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
